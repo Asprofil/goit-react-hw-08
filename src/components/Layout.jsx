52efeb224821c4f5d5c1c89b37d100fe2a82d8bc
Asprofil <adober.demchenko@gmail.com>
2024-08-31 23:34:36 +0300

    FA

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppBar } from './AppBar/Appbar';
 
+
 export const Layout = ({ children }) => {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
