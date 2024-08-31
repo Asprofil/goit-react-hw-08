c82d67d0a3e552789b06128a09accd521d92c5d0
Asprofil <adober.demchenko@gmail.com>
2024-08-31 23:15:50 +0300

    1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { AppBar } from '../components/AppBar/Appbar';
+import { AppBar } from './AppBar/AppBar';
 
 export const Layout = ({ children }) => {
   return (
